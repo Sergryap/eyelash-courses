91ce3e91eb31d420c5929b43ca9d49641189dbb9
Sergryap <rs1180@mail.ru>
2023-03-06 23:24:26 +0500

    The vk bot code decomposition

--- courses/management/commands/start_vk_bot.py
+++ courses/management/commands/start_vk_bot.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.core.management import BaseCommand
-from vk_bot.vk_bot import VkBot
+from vk_bot.vk_bot import handle_users_reply
 from vk_bot.mat_filter import MatFilter
-from vkwave.bots import SimpleBotEvent, DefaultRouter, simple_bot_message_handler
+from vkwave.bots import SimpleBotEvent,  SimpleLongPollBot, DefaultRouter, simple_bot_message_handler
 
 
 class Command(BaseCommand):
@@ -17,11 +17,11 @@ class Command(BaseCommand):
 def start_vk_bot():
 
     router = DefaultRouter()
-    bot = VkBot(settings.VK_TOKEN, settings.VK_GROUP_ID)
 
     @simple_bot_message_handler(router, MatFilter())
     async def basic_send(event: SimpleBotEvent):
-        await bot.handle_users_reply(event)
+        await handle_users_reply(event)
 
-    bot.updater.dispatcher.add_router(router)
-    bot.updater.run_forever()
+    updater = SimpleLongPollBot(tokens=settings.VK_TOKEN, group_id=settings.VK_GROUP_ID)
+    updater.dispatcher.add_router(router)
+    updater.run_forever()
--- vk_bot/vk_bot.py
+++ vk_bot/vk_bot.py
@@ -1,46 +1,43 @@
 from asgiref.sync import sync_to_async
-from vkwave.bots import SimpleBotEvent, SimpleLongPollBot
+from vkwave.bots import SimpleBotEvent
 from vkwave.bots.storage.types import Key
 from vkwave.bots.storage.storages import Storage
 from courses.models import Client
-from vk_bot import handlers
+from . import handlers
 
 
-class VkBot:
-
+async def handle_users_reply(event: SimpleBotEvent):
     storage = Storage()
+    user_id = event.user_id
+    api = event.api_ctx
+
     states_functions = {
         'START': handlers.start,
+
+
     }
 
-    def __init__(self, token, group_id):
-        self.updater = SimpleLongPollBot(tokens=token, group_id=group_id)
+    if not await storage.contains(Key(f'{user_id}_first_name')):
+        user_data = (await api.users.get(user_ids=user_id)).response[0]
+        await storage.put(Key(f'{user_id}_first_name'), user_data.first_name)
+        await storage.put(Key(f'{user_id}_last_name'), user_data.last_name)
 
-    @classmethod
-    async def handle_users_reply(cls, event: SimpleBotEvent):
-        user_id = event.user_id
-        api = event.api_ctx
+    user, _ = await Client.objects.aget_or_create(
+        vk_id=user_id,
+        defaults={
+            'first_name': await storage.get(Key(f'{user_id}_first_name')),
+            'last_name': await storage.get(Key(f'{user_id}_last_name')),
+            'vk_profile': f'https://vk.com/id{user_id}'
+        }
+    )
+    if not await storage.contains(Key(f'{user_id}_instance')):
+        await storage.put(Key(f'{user_id}_instance'), user)
 
-        if not await cls.storage.contains(Key(f'{user_id}_first_name')):
-            user_data = (await api.users.get(user_ids=user_id)).response[0]
-            await cls.storage.put(Key(f'{user_id}_first_name'), user_data.first_name)
-            await cls.storage.put(Key(f'{user_id}_last_name'), user_data.last_name)
+    if event.text.lower().strip() in ['start', '/start', 'начать', 'старт']:
+        user_state = 'START'
+    else:
+        user_state = user.bot_state
 
-        user, _ = await Client.objects.aget_or_create(
-            vk_id=user_id,
-            defaults={
-                'first_name': await cls.storage.get(Key(f'{user_id}_first_name')),
-                'last_name': await cls.storage.get(Key(f'{user_id}_last_name')),
-                'vk_profile': f'https://vk.com/id{user_id}'
-            }
-        )
-        await cls.storage.put(Key(f'{user_id}_instance'), user)
-
-        if event.text.lower().strip() in ['start', '/start', 'начать', 'старт']:
-            user_state = 'START'
-        else:
-            user_state = user.bot_state
-
-        state_handler = cls.states_functions[user_state]
-        user.bot_state = await state_handler(event, cls.storage)
-        await sync_to_async(user.save)()
+    state_handler = states_functions[user_state]
+    user.bot_state = await state_handler(event, storage)
+    await sync_to_async(user.save)()
