9b7dfa9d63eaee1a9ed7efa6a9fb889d23628936
Sergryap <rs1180@mail.ru>
2023-03-17 19:54:25 +0500

    Added postgres db

--- eyelash_courses/settings.py
+++ eyelash_courses/settings.py
@@ -76,11 +76,19 @@ WSGI_APPLICATION = 'eyelash_courses.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        env='DB_URL',
+        conn_max_age=600
+    ),
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
