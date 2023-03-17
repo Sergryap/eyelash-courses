455db14e157242dc17c47b4ba3f59be62523bfde
Sergryap <rs1180@mail.ru>
2023-03-18 00:36:04 +0500

    Modified deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,7 @@ set -Eeuo pipefail
 cd /
 cd /opt/eyelash-courses
 git pull
-if ! [ -e venv ]
-then
-python3 -m venv venv
-fi
-source venv/bin/activate
-pip install -r requirements.txt
-pip install gunicorn
-if [ -e static ]
-then
-sudo rm -rf static
-fi
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate --noinput
 sudo systemctl daemon-reload
+sudo systemctl restart django-bot-example.service
+sudo systemctl restart django-bot-start.service
 echo "Deploy completed successfully!"
