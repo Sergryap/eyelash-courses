0bc98cd653cdf5cf1670110612765685ee45d3eb
Sergryap <rs1180@mail.ru>
2023-03-26 16:13:25 +0500

    Added start_all_vk_bot.py

---
+++ courses/management/commands/start_all_vk_bot.py
@@ -0,0 +1,25 @@
+import logging
+import asyncio
+
+from django.core.management import BaseCommand
+from vk_bot.async_longpoll import listen_server as vk_listen_server
+from tg_bot.tg_bot import listen_server as tg_listen_server
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        try:
+            start_vk_bot()
+        except Exception as exc:
+            print(exc)
+            raise
+
+
+def start_vk_bot():
+    logger = logging.getLogger('telegram')
+    logger.warning('VK-Бот "eyelash-courses" запущен')
+
+    loop = asyncio.get_event_loop()
+    asyncio.ensure_future(vk_listen_server())
+    asyncio.ensure_future(tg_listen_server())
+    loop.run_forever()
