5353b7dadbdae0d60d2099af3f8d291d46f960d3
Sergryap <rs1180@mail.ru>
2023-04-02 21:05:54 +0500

    Added 'contact' page

--- courses/views.py
+++ courses/views.py
@@ -10,3 +10,8 @@ def home(request):
 def about(request):
     template = 'courses/about.html'
     return render(request, template)
+
+
+def contact(request):
+    template = 'courses/contact.html'
+    return render(request, template)
