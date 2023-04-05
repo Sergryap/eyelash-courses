23f98e126e62dbad5a9652dd3f26386455653314
Sergryap <rs1180@mail.ru>
2023-04-05 18:54:49 +0500

    modified course-details.html

--- courses/views.py
+++ courses/views.py
@@ -49,9 +49,11 @@ def course(request):
 
 def course_details(request, slug: str):
     template = 'courses/course-details.html'
-    course_instance = Course.objects.prefetch_related('images').get(slug=slug)
+    course_instance = Course.objects.select_related('lecture').prefetch_related('images').get(slug=slug)
     context = {
+        'participants': max(course_instance.get_count_participants(), 2),
         'course': course_instance,
+        'date': course_instance.scheduled_at.strftime("%d.%m.%Y"),
         'images': [
             {
                 'url': image.image.url, 'number': number
