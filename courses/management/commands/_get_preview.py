e02d8006a31a9e717d47f689449ee6107754c07a
sergryap <rs1180@mail.ru>
2023-04-22 17:34:18 +0500

    modified get_preview func

--- courses/management/commands/_get_preview.py
+++ courses/management/commands/_get_preview.py
@@ -5,13 +5,32 @@ from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 
-def get_preview(obj, attr='image_preview', width=130, height=130):
-    file_path = obj.image.path
-    new_filename_exclude_ext = f'{os.path.split(os.path.splitext(file_path)[0])[1]}_{attr}'
+def get_preview(
+        obj,
+        image_attr: str = 'image',
+        preview_attr='image_preview',
+        suffix: str = None,
+        width=130, height=130
+):
+
+    """
+    Создает превью изображения требуемого размера и записывает его в атрибут модели:
+
+    obj - экземпляр модели;
+    image_attr - атрибут модели, в котором находится исходное изображение;
+    preview_attr - атрибут модели, в который будет записано превью, должно быть типа ImageField;
+    suffix - для имени файла превью;
+    width - максимальная ширина превью;
+    height - максимальная ширина превью.
+    """
+
+    file_path = getattr(obj, image_attr).path
+    suffix = suffix if suffix is not None else f'_{preview_attr}'
+    new_filename_exclude_ext = f'{os.path.split(os.path.splitext(file_path)[0])[1]}{suffix}'
     new_filename = f'{new_filename_exclude_ext}.jpg'
     img = Image.open(file_path)
     remove_files = [file for file in os.listdir(os.path.split(file_path)[0]) if new_filename_exclude_ext in file]
-    if hasattr(obj, attr) and getattr(obj, attr) and os.path.isfile(getattr(obj, attr).path):
+    if hasattr(obj, preview_attr) and getattr(obj, preview_attr) and os.path.isfile(getattr(obj, preview_attr).path):
         for file in remove_files:
             print(f'Deleted: {file}')
             os.remove(os.path.join(os.path.split(file_path)[0], file))
@@ -20,7 +39,7 @@ def get_preview(obj, attr='image_preview', width=130, height=130):
     buffer = BytesIO()
     img.save(fp=buffer, format='JPEG')
     file_content = ContentFile(buffer.getvalue())
-    preview = getattr(obj, attr)
+    preview = getattr(obj, preview_attr)
     preview.save(
         new_filename,
         InMemoryUploadedFile(
