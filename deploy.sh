5285419c6fbd433d6f6a3ba5d9c04e9c90558d62
Sergryap <rs1180@mail.ru>
2023-04-30 15:04:55 +0500

    deploy.sh file has been modified

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,11 @@ then
 python3 -m venv venv
 fi
 source venv/bin/activate
+if ! [[ $# -eq 0 ]] && [ "$1" == "-r" ]
+then
+echo "Install dependencies"
+pip install -r requirements.txt
+fi
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 sudo systemctl daemon-reload
