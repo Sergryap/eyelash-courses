64ec237960ef8ece0d4e5e0302c01d05813a6769
Sergryap <rs1180@mail.ru>
2023-04-30 11:35:23 +0500

    The deploy.sh file has been modified

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,13 @@
 set -Eeuo pipefail
 cd /opt/eyelash-courses/
 git pull
+if ! [ -e venv ]
+then
+python3 -m venv venv
+fi
+source venv/bin/activate
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate --noinput
 sudo systemctl daemon-reload
 sudo systemctl restart django-bot-example.service
 sudo systemctl restart django-bot-start.service
