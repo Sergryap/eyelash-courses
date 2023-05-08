af3b98fe8f14502c3bd2fb1e78af7db75a66310d
Sergryap <rs1180@mail.ru>
2023-05-08 18:11:16 +0500

    start_all_bot command has been refactored

--- courses/management/commands/start_all_bot.py
+++ courses/management/commands/start_all_bot.py
@@ -1,9 +1,13 @@
 import logging
 import asyncio
 
+from django.conf import settings
 from django.core.management import BaseCommand
-from vk_bot.async_longpoll import listen_server as vk_listen_server
-from tg_bot.tg_bot import listen_server as tg_listen_server
+
+from vk_bot.async_longpoll import event_handler as vk_event_handler
+from tg_bot.tg_bot import handle_event as tg_event_handler
+from vk_bot.longpollserver import LongPollServer
+from tg_bot.tglongpollserver import TgLongPollServer
 
 
 class Command(BaseCommand):
@@ -19,7 +23,19 @@ def start_vk_bot():
     logger = logging.getLogger('telegram')
     logger.warning('Боты VK и TG "eyelash-courses" запущены')
 
+    vk_connect = LongPollServer(
+        vk_group_token=settings.VK_TOKEN,
+        redis_db=settings.REDIS_DB,
+        group_id=settings.VK_GROUP_ID,
+        handle_event=vk_event_handler
+    )
+    tg_connect = TgLongPollServer(
+        tg_token=settings.TG_TOKEN,
+        redis_db=settings.REDIS_DB,
+        handle_event=tg_event_handler
+    )
+
     loop = asyncio.get_event_loop()
-    asyncio.ensure_future(vk_listen_server())
-    asyncio.ensure_future(tg_listen_server())
+    for connect in [vk_connect, tg_connect]:
+        asyncio.ensure_future(connect.listen_server())
     loop.run_forever()
