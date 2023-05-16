b3feace6285717d156954e1ef347afe9a8643b3f
Sergryap <rs1180@mail.ru>
2023-05-16 20:03:14 +0500

    update_message_sending_tasks implementation

--- bots/tg_bot/tg_api.py
+++ bots/tg_bot/tg_api.py
@@ -3,6 +3,12 @@ import json
 import aiohttp
 import redis
 
+from asgiref.sync import sync_to_async
+from django.utils import timezone
+from courses.models import Course, Office
+from typing import Dict
+from textwrap import dedent
+
 
 class TgApi:
     """Класс API методов Tg"""
@@ -11,6 +17,7 @@ class TgApi:
         self.token = tg_token
         self.redis_db = redis_db
         self.loop = loop
+        self.sending_tasks = None
 
     async def send_message(self, chat_id, msg, *, reply_markup=None, parse_mode=None):
         """Отправка сообщения через api TG"""
@@ -28,6 +35,19 @@ class TgApi:
             res.raise_for_status()
             return json.loads(await res.text())
 
+    @staticmethod
+    async def create_reminder_text(first_name: str, course: Course, office: Office) -> str:
+        return f'''
+             {first_name}, напоминаем,
+             что вы записаны на курс:
+             *{course.name.upper()}*
+             _Дата курса: {course.scheduled_at.strftime("%d.%m.%Y")}._
+             _Время начала: {course.scheduled_at.strftime("%H:%M")}._
+             _Адрес: {office.address}_
+             _Спасибо, что выбрали нашу школу._
+             _Будем рады вас видеть!_
+             '''
+
     async def send_message_later(
             self,
             chat_id,
@@ -39,7 +59,7 @@ class TgApi:
             remind_before: int = None,
             reply_markup=None,
             parse_mode=None
-    ):
+    ) -> asyncio.Task:
         """Отложенная отправка сообщения"""
         timer = interval if interval else time_to_start - time_offset - remind_before
 
@@ -47,6 +67,40 @@ class TgApi:
             await asyncio.sleep(timer)
             await self.send_message(chat_id, msg, reply_markup=reply_markup, parse_mode=parse_mode)
         return asyncio.ensure_future(coro(), loop=self.loop)
+
+    async def update_message_sending_tasks(
+            self,
+            time_offset: int = 5 * 3600,
+            remind_before: int = 86400 - 6 * 3600,
+            reminder_text: str = None
+    ) -> Dict[str, asyncio.Task]:
+
+        """Создание отложенных задач по отправке сообщений пользователям по данным базы данных"""
+
+        future_courses = await Course.objects.async_filter(scheduled_at__gt=timezone.now(), published_in_bot=True)
+        future_courses_prefetch = await sync_to_async(future_courses.prefetch_related)('clients')
+        office = await Office.objects.async_first()
+        tasks = {}
+        for course in future_courses_prefetch:
+            clients = await sync_to_async(course.clients.all)()
+            time_to_start = (course.scheduled_at - timezone.now()).total_seconds()
+            interval = time_to_start - time_offset - remind_before
+            if interval < 0:
+                continue
+            for client in clients:
+                if not client.telegram_id:
+                    continue
+                text = await self.create_reminder_text(client.first_name, course, office)
+                msg = reminder_text if reminder_text else text
+                name_task = f'remind_record_tg_{client.telegram_id}_{course.pk}'
+                task = await self.send_message_later(
+                    client.telegram_id,
+                    dedent(msg),
+                    interval=interval,
+                    parse_mode='Markdown'
+                )
+                tasks.update({name_task: task})
+        return tasks
 
     async def send_location(self, chat_id, *, lat, long, reply_markup=None):
         """Отправка локации через api TG"""
