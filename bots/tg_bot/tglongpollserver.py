4e3af48b93ce7f16da46f47c8cebc9cb96a3ebe5
sergryap <rs1180@mail.ru>
2023-05-28 14:38:12 +0500

    Update task create intervals

--- bots/tg_bot/tglongpollserver.py
+++ bots/tg_bot/tglongpollserver.py
@@ -16,7 +16,7 @@ class TgLongPollServer(LongPollServer):
 
     async def listen_server(self, *, loop=None):
         async with StartAsyncSession(self):
-            await self.api.create_tasks_from_db(hour_timers=[4, 16, 22])
+            await self.api.create_tasks_from_db(hour_timers=[3, 6, 9, 12, 15, 18, 22])
             await self.api.bypass_users_to_create_tasks(hour_timers=[3, 15, 21])
             while True:
                 async with UpdateTgEventSession(self):
