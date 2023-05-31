557a977d557a99255ed99149e25bb30a892c7071
Sergryap <rs1180@mail.ru>
2023-05-31 17:09:33 +0500

    The clean_course_images command was added

--- courses/management/commands/_clean_course_images.py
+++ courses/management/commands/_clean_course_images.py
@@ -14,7 +14,7 @@ def clean_course_images():
         image_path.path
         for course in courses
         for image in course.images.all()
-        for image_path in (image.image, image.image, image.image_preview)
+        for image_path in (image.image, image.big_preview, image.image_preview)
     }
     for file in all_directory_files.difference(course_files):
         print(f'Удаление файла: {file}')
